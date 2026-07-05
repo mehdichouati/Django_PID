49b06b202de108720f2df3aca14e8d8f17a77207
mehdichouati <mehdicb7@gmail.com>
2026-07-05 17:18:37 +0200

    Itération 2 : modèle Artist + migration

--- reservations/catalogue/models.py
+++ reservations/catalogue/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Artist(models.Model):
+    firstname = models.CharField(max_length=60)
+    lastname = models.CharField(max_length=60)
+
+    class Meta:
+        db_table = 'artists'
+
+    def __str__(self):
+        return f"{self.firstname} {self.lastname}"
