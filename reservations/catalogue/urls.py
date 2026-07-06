f1526471e5c45581e68cfadb8dcd50f2b520bd47
mehdichouati <mehdicb7@gmail.com>
2026-07-06 16:36:39 +0200

    Itération 3 : formulaires CRUD complets pour Artist (create/edit/delete)

--- reservations/catalogue/urls.py
+++ reservations/catalogue/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.artist_index, name='artist_index'),
+    path('create', views.artist_create, name='artist_create'),
+    path('store', views.artist_store, name='artist_store'),
+    path('edit/<int:id>', views.artist_edit, name='artist_edit'),
+    path('update/<int:id>', views.artist_update, name='artist_update'),
+    path('delete/<int:id>', views.artist_delete, name='artist_delete'),
     path('<int:id>', views.artist_show, name='artist_show'),
 ]
--- reservations/catalogue/views.py
+++ reservations/catalogue/views.py
@@ -1,8 +1,6 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Artist
+from .forms import ArtistForm
 
 
 def artist_index(request):
@@ -13,3 +11,37 @@ def artist_index(request):
 def artist_show(request, id):
     artist = get_object_or_404(Artist, id=id)
     return render(request, 'catalogue/artist_show.html', {'artist': artist})
+
+
+def artist_create(request):
+    form = ArtistForm()
+    return render(request, 'catalogue/artist_form.html', {'form': form, 'title': 'Nouvel artiste'})
+
+
+def artist_store(request):
+    form = ArtistForm(request.POST)
+    if form.is_valid():
+        form.save()
+        return redirect('artist_index')
+    return render(request, 'catalogue/artist_form.html', {'form': form, 'title': 'Nouvel artiste'})
+
+
+def artist_edit(request, id):
+    artist = get_object_or_404(Artist, id=id)
+    form = ArtistForm(instance=artist)
+    return render(request, 'catalogue/artist_form.html', {'form': form, 'title': 'Modifier l\'artiste'})
+
+
+def artist_update(request, id):
+    artist = get_object_or_404(Artist, id=id)
+    form = ArtistForm(request.POST, instance=artist)
+    if form.is_valid():
+        form.save()
+        return redirect('artist_show', id=artist.id)
+    return render(request, 'catalogue/artist_form.html', {'form': form, 'title': 'Modifier l\'artiste'})
+
+
+def artist_delete(request, id):
+    artist = get_object_or_404(Artist, id=id)
+    artist.delete()
+    return redirect('artist_index')
