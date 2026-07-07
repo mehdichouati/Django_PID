3d113abfd5a91e3dba96d6647eae05bf85214a01
mehdichouati <mehdicb7@gmail.com>
2026-07-07 15:13:09 +0200

    Itération 4 : modèles Role, RoleUser, UserMeta + migration

--- reservations/catalogue/models.py
+++ reservations/catalogue/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Artist(models.Model):
@@ -10,3 +11,32 @@ class Artist(models.Model):
 
     def __str__(self):
         return f"{self.firstname} {self.lastname}"
+
+
+class Role(models.Model):
+    role = models.CharField(max_length=30)
+
+    class Meta:
+        db_table = 'roles'
+
+    def __str__(self):
+        return self.role
+
+
+class RoleUser(models.Model):
+    role = models.ForeignKey(Role, on_delete=models.CASCADE, db_column='role_id')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, db_column='user_id')
+
+    class Meta:
+        db_table = 'role_user'
+
+
+class UserMeta(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='meta')
+    langue = models.CharField(max_length=2, default='FR')
+
+    class Meta:
+        db_table = 'user_meta'
+
+    def __str__(self):
+        return f"Meta de {self.user.username}"
