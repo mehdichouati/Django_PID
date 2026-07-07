d58e03396df323641ad5da848be3e58a541a8c06
mehdichouati <mehdicb7@gmail.com>
2026-07-07 15:39:14 +0200

    Itération 4 terminée : récupération de mot de passe fonctionnelle (flux complet testé)

--- reservations/reservations/urls.py
+++ reservations/reservations/urls.py
@@ -9,4 +9,20 @@ urlpatterns = [
     path('register/', catalogue_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='catalogue/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='artist_index'), name='logout'),
+
+    path('password-reset/', auth_views.PasswordResetView.as_view(
+        template_name='catalogue/password_reset_form.html'
+    ), name='password_reset'),
+
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='catalogue/password_reset_done.html'
+    ), name='password_reset_done'),
+
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='catalogue/password_reset_confirm.html'
+    ), name='password_reset_confirm'),
+
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='catalogue/password_reset_complete.html'
+    ), name='password_reset_complete'),
 ]
