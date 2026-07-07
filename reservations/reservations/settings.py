22b5919409d6064a778284582f94391cafe1f687
mehdichouati <mehdicb7@gmail.com>
2026-07-07 15:23:40 +0200

    Correction LOGIN_REDIRECT_URL après connexion

--- reservations/reservations/settings.py
+++ reservations/reservations/settings.py
@@ -125,3 +125,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Authentication redirects
+LOGIN_REDIRECT_URL = 'artist_index'
+LOGOUT_REDIRECT_URL = 'artist_index'
+LOGIN_URL = 'login'
