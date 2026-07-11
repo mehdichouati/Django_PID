9853803a117731a3756ba78e894be0d801620b46
mehdichouati <mehdicb7@gmail.com>
2026-07-11 16:35:28 +0200

    Back-office admin : enregistrement de tous les modèles dans Django Admin

--- reservations/catalogue/admin.py
+++ reservations/catalogue/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import (
+    Artist, Role, RoleUser, UserMeta, Type, ArtisteType,
+    Locality, Location, Show, ArtisteTypeShow, Representation,
+    Price, Reservation, RepresentationReservation, Review
+)
 
-# Register your models here.
+admin.site.register(Artist)
+admin.site.register(Role)
+admin.site.register(RoleUser)
+admin.site.register(UserMeta)
+admin.site.register(Type)
+admin.site.register(ArtisteType)
+admin.site.register(Locality)
+admin.site.register(Location)
+admin.site.register(Show)
+admin.site.register(ArtisteTypeShow)
+admin.site.register(Representation)
+admin.site.register(Price)
+admin.site.register(Reservation)
+admin.site.register(RepresentationReservation)
+admin.site.register(Review)
